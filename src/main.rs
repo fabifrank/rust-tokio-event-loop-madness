b7e0449333b552cc00ce9b6567e2284fe79e325c
fabifrank <info@fabifrank.com>
2019-06-20 09:35:36 +0200

    add handler to test if post request is sent

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ pub fn listen() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .data(web::JsonConfig::default().limit(4096))
             .service(web::resource("/push").route(web::post().to(index)))
+            .service(web::resource("/test").route(web::post().to(test)))
     })
     .bind("127.0.0.1:8080")?
     .run()
@@ -36,15 +37,23 @@ fn index(item: web::Json<Value>) -> HttpResponse {
     HttpResponse::Ok().json(item.0) // <- send response
 }
 
+fn test(item: web::Json<Value>) -> HttpResponse {
+    println!("recevied test call!");
+    println!("{:?}", &item);
+    
+    HttpResponse::Ok().json(item.0) // <- send response
+}
+
 
 
 pub fn send(mut data: serde_json::Value) {
     println!("# Start running log post future...");
 
+    // if the following line is removed, the call is not received by the test function above
     System::new("test").block_on(lazy(|| {
         let req = Request::builder()
             .method("POST")
-            .uri("http://localhost:8888")
+            .uri("http://localhost:8080/test")
             .body(Body::from(data.to_string()))
             .expect("request builder");
 
